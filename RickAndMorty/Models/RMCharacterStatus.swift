47f0a3a8d7ee5e581ebee89fbae014fd1f5e6f12
murad <muradkv@gmail.com>
2023-05-08 22:50:59 +0300

    Add models

---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by murad on 08.05.2023.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by murad on 08.05.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
